b2dcaf21ea335af7ca20170a4bdb7b3de29cfc35
Michele Ghirardelli <50736672+ghirailghiro@users.noreply.github.com>
2022-11-11 13:30:39 +0000

    commenti

--- sql_batch_update.py
+++ sql_batch_update.py
@@ -6,14 +6,29 @@ from batch_extractor import Batch_Extractor, abstractmethod
 import os
 import oracledb
 
-'''Che parametri inserire per fare in modo di usarlo -> get_data'''
-'''defin the return type'''
+'''La classe astratta è definita con tre metodi:
+- Uno da utilizzare per la instaurare la connessione con la fonte dati SQL
+- Uno per estrarre i dati da una fonte dati SQL
+- Uno per andare a scrivere dati su una fonte SQL
+'''
 class Batch_Extractor(Batch_Extractor):
+    '''
+    Il seguente metodo non prende parametri in input,
+    ma come output un connettore al db
+    '''
     @abstractmethod
     def get_connection(self):
         pass
+    '''
+    Il seguente metodo prende in input i valori per comporre una query:
+        - select: le colonne da estrarre dalla query
+        - from: la table da cui estrarre
+        - where: i filtri per la 
+        - limit
+    come output i risultati della query
+    '''
     @abstractmethod
-    def get_data(self,connection,query):
+    def get_data(self,connection,select,from,where,limit = None):
         pass
     @abstractmethod
     def write_data(self,connection,data,where):
@@ -29,9 +44,12 @@ class OneStreamExtractor(Batch_Extractor):
         port = os.environ.get('PORT')
         return  oracledb.connect(user=user, password=userpwd,host=server, port=port, service_name=db)
 
-    def get_data(self,connection,query):
+    def get_data(self,connection,select,from,where,limit):
+        if(limit == None):
+            query= "SELECT "+select+" FROM "+from+" WHERE "+ where
+        else:
+            query= "SELECT "+select+" FROM "+from+" WHERE "+ where + " LIMIT "+limit
         results = connection.execute(query)
-        '''Cosa ritornare??'''
         return results
         pass
     def write_data(self,connection,data,where):
@@ -43,6 +61,4 @@ class OneStreamExtractor(Batch_Extractor):
         for error in cur.getbatcherrors():
             listOfErrors.append("Error"+ error.message.rstrip()+ "at row offset"+ error.offset)
         return (listOfErrors > 0,listOfErrors)
-        '''Cosa usare per scrivere?'''
-        '''Che tipo di data deve dare in input?'''
         pass
